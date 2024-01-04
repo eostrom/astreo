71900fa27851172b115a13146806b7d3bfd33d68
Erik Ostrom <code@erikostrom.com>
2024-01-03 22:42:06 -0600

    Eliminate ESLint/Prettier conflict

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,7 +4,11 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
   overrides: [
     {
       env: {
@@ -22,10 +26,4 @@ module.exports = {
     sourceType: 'module',
   },
   plugins: ['@typescript-eslint'],
-  rules: {
-    indent: ['error', 4],
-    'linebreak-style': ['error', 'unix'],
-    quotes: ['error', 'single'],
-    semi: ['error', 'never'],
-  },
 }
