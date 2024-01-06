c13775e50f0d01f804d6c0390a4863b2b1cfc2d6
Erik Ostrom <code@erikostrom.com>
2024-01-06 17:34:31 -0600

    Port home page markup from Gridsome
    Including two Vue components, as-is.

---
+++ src/components/ArtsContents.vue
@@ -0,0 +1,16 @@
+<template>
+  <nav>
+    <ul class='contents'>
+      <li><a class='tk'>works</a></li>
+      <li>
+        <a href='/arts/words/'>words</a>
+      </li>
+      <li>
+        <a href='/arts/links/'>links</a>
+      </li>
+      <li>
+        <a href='/arts/shows/'>shows</a>
+      </li>
+    </ul>
+  </nav>
+</template>
---
+++ src/components/CodeContents.vue
@@ -0,0 +1,15 @@
+<template>
+  <nav>
+    <ul class="contents">
+      <li>
+        <a href='/code/works/'>works</a>
+      </li>
+      <li>
+        <a href='/code/words/'>words</a>
+      </li>
+      <li>
+        <a href='/code/links/'>links</a>
+      </li>
+    </ul>
+  </nav>
+</template>
