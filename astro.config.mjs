fe9d8631dc3d0a962933843a55a5660b37c14248
Erik Ostrom <code@erikostrom.com>
2024-01-14 20:22:51 -0600

    Code post pages.
    Duplicated code from 978dd43258e80ed90912e31408bd3cb8d585e12d, with the same caveats.
    
    Imported Night Owl theme for Shiki and left-justified text (mostly for code segments).

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
 import {defineConfig} from 'astro/config'
 import vue from '@astrojs/vue'
+import nightOwlShikiTheme from './config/Night Owl-color-theme.json'
 
 // https://astro.build/config
-export default defineConfig({integrations: [vue()]})
+export default defineConfig({
+  integrations: [vue()],
+  markdown: {
+    shikiConfig: {
+      theme: nightOwlShikiTheme
+    }
+  }
+})
